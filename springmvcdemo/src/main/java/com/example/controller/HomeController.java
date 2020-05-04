f35846a9824e4cbfe22952d6efb87df54c0f46b4
RESTY NASIMBWA <54865360+restynan@users.noreply.github.com>
2020-05-03 22:58:41 -0500

    first demo

---
+++ springmvcdemo/src/main/java/com/example/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String welcome(){
+        return "welcome";
+    }
+}
